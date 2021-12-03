7e383db0bea7a47e7e59770ac9c15bf155b9e4eb
Guillaume Lam <guillaume.lam10100@gmail.com>
2021-12-03 17:18:21 -0500

    smth

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,6 @@
+julia run.jl
 cp CNAME site/
+git add .
+git commit -m "[AUTO] Adding latest site"
 #git push origin `git subtree split --prefix site origin gh-pages`:gh-pages --force
 git subtree push --prefix site origin gh-pages
